a31502e3f6299dcb2bf2f9ae76af19f61649ff82
dan sinclair <dj2@everburning.com>
2023-08-21 19:37:20 -0400

    Add depthSlice to WGPURenderPassColorAttachment
    Fixes #215

--- webgpu.h
+++ webgpu.h
@@ -1234,6 +1234,7 @@ typedef struct WGPUProgrammableStageDescriptor {
 typedef struct WGPURenderPassColorAttachment {
     WGPUChainedStruct const * nextInChain;
     WGPU_NULLABLE WGPUTextureView view;
+    uint32_t depthSlice;
     WGPU_NULLABLE WGPUTextureView resolveTarget;
     WGPULoadOp loadOp;
     WGPUStoreOp storeOp;
