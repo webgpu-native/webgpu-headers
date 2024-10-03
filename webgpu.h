dda6175486b70a90f594bea136c2f3129e3ee1d7
Kai Ninomiya <kainino@chromium.org>
2024-10-02 20:03:52 -0700

    reorder some enums to match spec

--- webgpu.h
+++ webgpu.h
@@ -279,9 +279,9 @@ typedef enum WGPUAddressMode {
      * No value. See @ref SentinelValues.
      */
     WGPUAddressMode_Undefined = 0x00000000,
-    WGPUAddressMode_Repeat = 0x00000001,
-    WGPUAddressMode_MirrorRepeat = 0x00000002,
-    WGPUAddressMode_ClampToEdge = 0x00000003,
+    WGPUAddressMode_ClampToEdge = 0x00000001,
+    WGPUAddressMode_Repeat = 0x00000002,
+    WGPUAddressMode_MirrorRepeat = 0x00000003,
     WGPUAddressMode_Force32 = 0x7FFFFFFF
 } WGPUAddressMode WGPU_ENUM_ATTRIBUTE;
 
@@ -404,11 +404,11 @@ typedef enum WGPUCompareFunction {
     WGPUCompareFunction_Undefined = 0x00000000,
     WGPUCompareFunction_Never = 0x00000001,
     WGPUCompareFunction_Less = 0x00000002,
-    WGPUCompareFunction_LessEqual = 0x00000003,
-    WGPUCompareFunction_Greater = 0x00000004,
-    WGPUCompareFunction_GreaterEqual = 0x00000005,
-    WGPUCompareFunction_Equal = 0x00000006,
-    WGPUCompareFunction_NotEqual = 0x00000007,
+    WGPUCompareFunction_Equal = 0x00000003,
+    WGPUCompareFunction_LessEqual = 0x00000004,
+    WGPUCompareFunction_Greater = 0x00000005,
+    WGPUCompareFunction_NotEqual = 0x00000006,
+    WGPUCompareFunction_GreaterEqual = 0x00000007,
     WGPUCompareFunction_Always = 0x00000008,
     WGPUCompareFunction_Force32 = 0x7FFFFFFF
 } WGPUCompareFunction WGPU_ENUM_ATTRIBUTE;
