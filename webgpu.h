e806fa1827a95f9c2f691f3f86de35ddf0de0749
Kai Ninomiya <kainino@chromium.org>
2024-12-03 22:36:08 -0500

    Add PostTask hook
    Very rough first draft

--- webgpu.h
+++ webgpu.h
@@ -274,6 +274,7 @@ struct WGPURequestAdapterCallbackInfo;
 struct WGPURequestDeviceCallbackInfo;
 struct WGPUUncapturedErrorCallbackInfo;
 
+struct WGPUPostTaskCallbackInfo;
 
 /**
  * \defgroup Enumerations Enumerations
@@ -1164,6 +1165,14 @@ static const WGPUColorWriteMask WGPUColorWriteMask_Blue = 0x0000000000000004;
 static const WGPUColorWriteMask WGPUColorWriteMask_Alpha = 0x0000000000000008;
 static const WGPUColorWriteMask WGPUColorWriteMask_All = 0x000000000000000F /* Red | Green | Blue | Alpha */;
 
+/**
+ * Flags indicating types of work contained in the task.
+ */
+typedef WGPUFlags WGPUImplementationTaskFlags;
+static const WGPUImplementationTaskFlags WGPUImplementationTaskFlags_None = 0x0000000000000000;
+static const WGPUImplementationTaskFlags WGPUImplementationTaskFlags_CPUWork = 0x0000000000000001;
+static const WGPUImplementationTaskFlags WGPUImplementationTaskFlags_BlockingIO = 0x0000000000000002;
+
 typedef WGPUFlags WGPUMapMode;
 static const WGPUMapMode WGPUMapMode_None = 0x0000000000000000;
 static const WGPUMapMode WGPUMapMode_Read = 0x0000000000000001;
@@ -1507,6 +1516,15 @@ typedef struct WGPUUncapturedErrorCallbackInfo {
     /*.userdata1=*/NULL _wgpu_COMMA \
     /*.userdata2=*/NULL _wgpu_COMMA \
 })
+
+typedef struct WGPUPostTaskHookInfo {
+    WGPUPostTaskHook hook;
+    WGPU_NULLABLE void* userdata1;
+    WGPU_NULLABLE void* userdata2;
+}
+
+typedef (*WGPUImplementationTaskFunction)(void* impldata);
+typedef (*WGPUPostTaskHook)(WGPUImplementationTaskFlags flags, WGPUImplementationTaskFunction task, void* impldata, void* userdata1, void* userdata2);
 
 /** @} */
 
@@ -3768,6 +3786,10 @@ typedef struct WGPUInstanceDescriptor {
      * The `INIT` macro sets this to @ref WGPU_INSTANCE_CAPABILITIES_INIT.
      */
     WGPUInstanceCapabilities features;
+    /**
+     * The `INIT` macro sets this to `NULL`.
+     */
+    WGPUPostTaskHookInfo postTaskHook;
 } WGPUInstanceDescriptor WGPU_STRUCTURE_ATTRIBUTE;
 
 /**
@@ -3776,6 +3798,7 @@ typedef struct WGPUInstanceDescriptor {
 #define WGPU_INSTANCE_DESCRIPTOR_INIT _wgpu_MAKE_INIT_STRUCT(WGPUInstanceDescriptor, { \
     /*.nextInChain=*/NULL _wgpu_COMMA \
     /*.features=*/WGPU_INSTANCE_CAPABILITIES_INIT _wgpu_COMMA \
+    /*.postTaskHook=*/NULL _wgpu_COMMA \
 })
 
 /**
